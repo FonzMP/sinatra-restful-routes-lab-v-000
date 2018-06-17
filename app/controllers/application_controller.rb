0c4a46b5853c1e6166ea2206d659f0d0c2f40490
Alex Wilson <fonzmp@gmail.com>
2018-06-17 06:04:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cooking_time: params[:cooking_time])
     binding.pry
-    redirect "/recipes/#{@recipe[:id]/show}"
+    redirect "/recipes/:id/show}"
   end
 
   get '/recipes/:id' do
