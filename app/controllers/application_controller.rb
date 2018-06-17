4754de39e682ec9e5030e9a7bd27388f0cbfd154
Alex Wilson <fonzmp@gmail.com>
2018-06-17 06:15:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.find(params[:id])
     @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     binding.pry
-    redirect '/recipes/#{@recipe.id}'
+    redirect '/recipes/#{@recipe.id}/show'
   end
 
   delete '/recipes/:id/delete' do
