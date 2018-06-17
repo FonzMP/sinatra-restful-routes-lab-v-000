5855939c50f78e018261624b56960b91a6e8f924
Alex Wilson <fonzmp@gmail.com>
2018-06-17 05:41:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/recipes/:id/delete' do
-    recipe = Recipe.find(params[:id]) 
+    recipe = Recipe.find(params[:id])
+    recipe.delete 
   end
 end
